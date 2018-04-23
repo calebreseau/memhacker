731bd27ad232c838557e5d9024bf6da3f47b9d1b
cal_ <calebreseau@gmail.com>
2018-04-23 22:02:18 +0200

    big cleanup: now using only one buffer, and added stop option

--- umem.pas
+++ umem.pas
@@ -22,24 +22,27 @@ var
 
 implementation
 
+
 function researchmem(pid:dword;value:string;valuetype:dword;vlength:dword):string;
 var
-  bufdword:dword;
-  buffloat:single;
-  bufqword:qword;
-  bufstring:array[0..15] of char;
+  buf:array[0..254] of byte;
   bytesread:ptruint;
   target:thandle;
   resp:string;
   i:integer;
   searchdata:tsearchdata;
+  alreadyopened:boolean;
+  readvalue:string;
 begin
   log('Enter researchmem');
+  fillchar(buf,255,0);
+  alreadyopened:=true;
   target:=getsysprocesshandle(pid);
   if target<1 then
   begin
-     log('didnt find handle with getsysprocesshandle, opening process');
-     target:=openprocess(process_vm_read,false,pid);
+     log('didnt find handle, opening process');
+     target:=openprocess(process_vm_read or process_vm_operation,false,pid);
+     alreadyopened:=false;
      log('openprocess handle: '+inttohex(target,4));
   end
   else  log('found handle: '+inttohex(target,4));
@@ -58,87 +61,37 @@ begin
   tdata(data^).searchdata.addrcount:=0;
   for i:=0 to searchdata.index do
   begin
+      if tdata(data^).searchdata.stop=true then
+      begin
+          log('Stopping search');
+          break;
+      end;
       tdata(data^).searchdata.pgcurr:=i;
       if length(searchdata.retaddrs[i])>2 then
       begin
           log('reading addr '+inttostr(i+1)+'/'+inttostr(searchdata.index+1)+': '+searchdata.retaddrs[i]);
-          if valuetype=vt_dword then
+          if readprocessmemory(target,pointer(strtoint64(searchdata.retaddrs[i])),@buf,vlength,bytesread) then
           begin
-             if readprocessmemory(target,pointer(strtoint64(searchdata.retaddrs[i])),@bufdword,vlength,bytesread) then
-             begin
-               //log('rpm ok, '+inttostr(bytesread)+' bytes read');
-               if inttostr(bufdword)=value then
-               begin
-                 addsaddr(searchdata.retaddrs[i]);
-                 log('found at: '+searchdata.retaddrs[i]);
-                 tdata(data^).searchdata.addrcount+=1;
-               end;
-             end
-             else
-             begin
-               resp:='';
-               log('error rpm: '+inttostr(getlasterror));
-             end;
-          end;
-          if valuetype=vt_float then
+            if valuetype=vt_dword then readvalue:=inttostr(dword((@buf)^));
+            if valuetype=vt_qword then readvalue:=inttostr(qword((@buf)^));
+            if valuetype=vt_float then readvalue:=floattostr(single((@buf)^));
+            if valuetype=vt_string then readvalue:=string((@buf)^);
+            if value=readvalue then
+            begin
+              addsaddr(stringreplace(searchdata.retaddrs[i],'$','',[rfreplaceall,rfignorecase]));
+              log('found at: '+searchdata.retaddrs[i]);
+              tdata(data^).searchdata.addrcount+=1;
+            end;
+          end
+          else
           begin
-             if readprocessmemory(target,pointer(strtoint64(searchdata.retaddrs[i])),@buffloat,vlength,bytesread) then
-             begin
-               //log('rpm ok, '+inttostr(bytesread)+' bytes read');
-               if floattostr(buffloat)=value then
-               begin
-                 addsaddr(searchdata.retaddrs[i]);
-                 log('found at: '+searchdata.retaddrs[i]);
-                 tdata(data^).searchdata.addrcount+=1;
-               end;
-             end
-             else
-             begin
-               resp:='';
-               log('error rpm: '+inttostr(getlasterror));
-             end;
-          end;
-          if valuetype=vt_qword then
-          begin
-             if readprocessmemory(target,pointer(strtoint64(searchdata.retaddrs[i])),@bufqword,vlength,bytesread) then
-             begin
-               //log('rpm ok, '+inttostr(bytesread)+' bytes read');
-               if inttostr(bufqword)=value then
-               begin
-                 addsaddr(searchdata.retaddrs[i]);
-                 log('found at: '+searchdata.retaddrs[i]);
-                 tdata(data^).searchdata.addrcount+=1;
-               end;
-             end
-             else
-             begin
-               resp:='';
-               log('error rpm: '+inttostr(getlasterror));
-             end;
-          end;
-          if valuetype=vt_string then
-          begin
-             if readprocessmemory(target,pointer(strtoint64(searchdata.retaddrs[i])),@bufstring,vlength,bytesread) then
-             begin
-               //log('rpm ok, '+inttostr(bytesread)+' bytes read');
-               if bufstring=value then
-               begin
-                 addsaddr(searchdata.retaddrs[i]);
-                 log('found at: '+searchdata.retaddrs[i]);
-                 tdata(data^).searchdata.addrcount+=1;
-               end;
-             end
-             else
-             begin
-               resp:='';
-               log('error rpm: '+inttostr(getlasterror));
-             end;
+            log('error rpm: '+inttostr(getlasterror));
           end;
       end;
   end;
-  result:=resp;
+  result:='found '+inttostr(tdata(data^).searchdata.addrcount)+' addresses';
   log('result: '+result);
-  closehandle(target);
+  if not alreadyopened then closehandle(target);
   log('leaving researchmem');
 end;
 function searchmem(pid:dword;value:string;valuetype:dword;vlength:dword;startaddr:qword;endaddr:qword;advsearch:boolean):string;
@@ -147,18 +100,17 @@ var
   MemInfo: MEMORY_BASIC_INFORMATION;
   MemStart: pointer;
   i:qword;
-  bufdword:dword;
-  buffloat:single;
-  bufqword:qword;
-  bufstring:array[0..15] of char;
+  buf:array[0..254] of byte;
   bytesread:ptruint;
   ret:ptruint;
   tmp:string;
+  readvalue:string;
   j:integer;
   errcount:integer;
   meminfos:array of memory_basic_information;
   alreadyopened:boolean;
 begin
+    fillchar(buf,255,0);
     alreadyopened:=true;
     setlength(meminfos,0);
     errcount:=0;
@@ -194,6 +146,11 @@ begin
         and (meminfo.Protect<>PAGE_GUARD)
         and ((0<>meminfo.Protect and PAGE_READWRITE))
         then meminfos[high(meminfos)]:=meminfo;
+        if tdata(data^).searchdata.stop=true then
+        begin
+            break;
+            log('Stopping search');
+        end;
     end;
     log('found '+inttostr(length(meminfos))+' mem zones');
     tdata(data^).searchdata.addrcount:=0;
@@ -203,110 +160,61 @@ begin
     begin
       tdata(data^).searchdata.pgcurr:=j;
       i:=0;
+      if tdata(data^).searchdata.stop=true then
+      begin
+               log('Stopping search');
+               break;
+      end;
       while meminfos[j].baseaddress+meminfos[j].RegionSize>meminfos[j].baseaddress+i do
       begin
-          if valuetype=vt_dword then
-          begin
-             if readprocessmemory(target,pointer(meminfos[j].baseaddress+i),@bufdword,vlength,bytesread) then
-             begin
-               if value=inttostr(bufdword) then
+           if tdata(data^).searchdata.stop=true then
+           begin
+               log('Stopping search');
+               break;
+           end;
+           if readprocessmemory(target,pointer(meminfos[j].baseaddress+i),@buf,vlength,bytesread) then
+           begin
+               if valuetype=vt_dword then readvalue:=inttostr(dword((@buf)^));
+               if valuetype=vt_qword then readvalue:=inttostr(qword((@buf)^));
+               if valuetype=vt_float then readvalue:=floattostr(single((@buf)^));
+               if valuetype=vt_string then readvalue:=string((@buf)^);
+               if value=readvalue then
                begin
-                 tmp:=inttohex(qword(meminfos[j].baseaddress)+i,16);
-                 while tmp[1]='0' do delete(tmp,1,1);
-                 log('found at 0x'+tmp);
-                 tdata(data^).searchdata.addrcount+=1;
-                 addsaddr(tmp);
+                   tmp:=inttohex(qword(meminfos[j].baseaddress)+i,16);
+                   while tmp[1]='0' do delete(tmp,1,1);
+                   log('found at 0x'+tmp);
+                   tdata(data^).searchdata.addrcount+=1;
+                   addsaddr(tmp);
                end;
-             end
-             else
-             begin
+           end
+           else
+           begin
                log('error rpm: '+inttostr(getlasterror)+' at '+inttohex(qword(meminfos[j].baseaddress)+i,16));
                errcount+=1;
-             end;
-          end;
-          if valuetype=vt_float then
-          begin
-             if readprocessmemory(target,pointer(meminfos[j].baseaddress+i),@buffloat,vlength,bytesread) then
-             begin
-               if value=floattostr(buffloat) then
-               begin
-                 tmp:=inttohex(qword(meminfos[j].baseaddress)+i,16);
-                 while tmp[1]='0' do delete(tmp,1,1);
-                 log('found at 0x'+tmp);
-                 tdata(data^).searchdata.addrcount+=1;
-                 addsaddr(tmp);
-               end;
-             end
-             else
-             begin
-               log('error rpm: '+inttostr(getlasterror)+' at '+inttohex(qword(meminfos[j].baseaddress)+i,16));
-               errcount+=1;
-             end;
-          end;
-          if valuetype=vt_qword then
-          begin
-             if readprocessmemory(target,pointer(meminfos[j].baseaddress+i),@bufqword,vlength,bytesread) then
-             begin
-               if value=inttostr(bufqword) then
-               begin
-                 tmp:=inttohex(qword(meminfos[j].baseaddress)+i,16);
-                 while tmp[1]='0' do delete(tmp,1,1);
-                 log('found at 0x'+tmp);
-                 tdata(data^).searchdata.addrcount+=1;
-                 addsaddr(tmp);
-               end;
-             end
-             else
-             begin
-               log('error rpm: '+inttostr(getlasterror)+' at '+inttohex(qword(meminfos[j].baseaddress)+i,16));
-               errcount+=1;
-             end;
-          end;
-          if valuetype=vt_string then
-          begin
-             if readprocessmemory(target,pointer(meminfos[j].baseaddress+i),@bufstring,vlength,bytesread) then
-             begin
-               if value=bufstring then
-               begin
-                 tmp:=inttohex(qword(meminfos[j].baseaddress)+i,16);
-                 while tmp[1]='0' do delete(tmp,1,1);
-                 log('found at 0x'+tmp);
-                 tdata(data^).searchdata.addrcount+=1;
-                 addsaddr(tmp);
-               end;
-             end
-             else
-             begin
-               log('error rpm: '+inttostr(getlasterror)+' at '+inttohex(qword(meminfos[j].baseaddress)+i,16));
-               errcount+=1;
-             end;
-          end;
-          if advsearch then i+=1 else i+=vlength;
+           end;
+           if advsearch then i+=1 else i+=vlength;
       end;
     end;
     if not alreadyopened then closehandle(target);
-    result:='errcount: '+inttostr(errcount);
+    result:='found '+inttostr(tdata(data^).searchdata.addrcount)+' addresses';
     log('exit, result: '+result);
 end;
 
 function readmem(pid:dword;addr:qword;valuetype:dword;vlength:ptruint):string;
 var
-  bufdword:dword;
-  buffloat:single;
-  bufqword:qword;
-  bufstring:array[0..15] of char;
+  buf:array[0..254] of byte;
   bytesread:ptruint;
   target:thandle;
-  resp:string;
   alreadyopened:boolean;
 begin
+  fillchar(buf,255,0);
   alreadyopened:=true;
   log('Enter readmem');
   target:=getsysprocesshandle(pid);
   if target<1 then
   begin
      log('didnt find handle with getsysprocesshandle, opening process');
-     target:=openprocess(process_vm_read or process_query_information,false,pid);
+     target:=openprocess(process_vm_read or process_vm_operation,false,pid);
      alreadyopened:=false;
      log('openprocess handle: '+inttohex(target,4));
   end
@@ -317,79 +225,28 @@ begin
      result:='';
      exit;
   end;
-  if valuetype=vt_dword then
+  if readprocessmemory(target,pointer(addr),@buf,vlength,bytesread) then
   begin
-     log('valuetype=dword');
-     if readprocessmemory(target,pointer(addr),@bufdword,vlength,bytesread) then
-     begin
        log('rpm ok, '+inttostr(bytesread)+' bytes read');
-       resp:=inttostr(bufdword)
-     end
-     else
-     begin
-       resp:='';
-       log('error rpm: '+inttostr(getlasterror));
-     end;
-  end;
-  if valuetype=vt_float then
-  begin
-     log('valuetype=float');
-     if readprocessmemory(target,pointer(addr),@buffloat,vlength,bytesread) then
-     begin
-       log('rpm ok, '+inttostr(bytesread)+' bytes read');
-       resp:=floattostr(buffloat)
-     end
-     else
-     begin
-       resp:='';
-       log('error rpm: '+inttostr(getlasterror));
-     end;
-  end;
-  if valuetype=vt_qword then
-  begin
-     log('valuetype=qword');
-     if readprocessmemory(target,pointer(addr),@bufqword,vlength,bytesread) then
-     begin
-       log('rpm ok, '+inttostr(bytesread)+' bytes read');
-       resp:=inttostr(bufqword)
-     end
-     else
-     begin
-       resp:='';
-       log('error rpm: '+inttostr(getlasterror));
-     end;
-  end;
-  if valuetype=vt_string then
-  begin
-     log('valuetype=string');
-     if readprocessmemory(target,pointer(addr),@bufstring,vlength,bytesread) then
-     begin
-       log('rpm ok, '+inttostr(bytesread)+' bytes read');
-       resp:=bufstring
-     end
-     else
-     begin
-       resp:='';
-       log('error rpm: '+inttostr(getlasterror));
-     end;
-  end;
-  result:=resp;
-  log('result: '+result);
+       if valuetype=vt_dword then result:=inttostr(dword((@buf)^));
+       if valuetype=vt_qword then result:=inttostr(qword((@buf)^));
+       if valuetype=vt_float then result:=floattostr(single((@buf)^));
+       if valuetype=vt_string then result:=string((@buf)^);
+       log('result: '+result);
+  end
+  else log('error rpm: '+inttostr(getlasterror));
   if not alreadyopened then closehandle(target);
-  log('leaving readmem');
+  log('exit readmem');
 end;
 
 function writemem(pid:dword;addr:qword;valuetype:dword;value:string;vlength:ptruint):string;
 var
-  bufdword:dword;
-  buffloat:single;
-  bufqword:qword;
-  bufstring:array[0..15] of char;
+  buf:array[0..254] of byte;
   byteswritten:ptruint;
   target:thandle;
-  resp:string;
   alreadyopened:boolean;
 begin
+  fillchar(buf,255,0);
   alreadyopened:=true;
   log('Enter writemem');
   target:=getsysprocesshandle(pid);
@@ -407,70 +264,18 @@ begin
      result:='';
      exit;
   end;
-  if valuetype=vt_dword then
+  if valuetype=vt_dword then move(strtoint(value),buf,vlength);
+  if valuetype=vt_qword then move(strtoint64(value),buf,vlength);
+  if valuetype=vt_float then move(strtoint(value),buf,vlength);
+  if valuetype=vt_string then move(value,buf,vlength);
+  if writeprocessmemory(target,pointer(addr),@buf,vlength,byteswritten) then
   begin
-     log('valuetype=dword');
-     bufdword:=strtoint(value);
-     if writeprocessmemory(target,pointer(addr),@bufdword,vlength,byteswritten) then
-     begin
        log('wpm ok, '+inttostr(byteswritten)+' bytes written');
-       resp:='ok'
-     end
-     else
-     begin
-       log('wpm error'+inttostr(getlasterror)+', '+inttostr(byteswritten)+' bytes written');
-       resp:='error '+inttostr(getlasterror);
-     end;
-  end;
-  if valuetype=vt_float then
-  begin
-     log('valuetype=float');
-     buffloat:=strtofloat(value);
-     if writeprocessmemory(target,pointer(addr),@buffloat,vlength,byteswritten) then
-     begin
-       log('wpm ok, '+inttostr(byteswritten)+' bytes written');
-       resp:='ok'
-     end
-     else
-     begin
-       log('wpm error'+inttostr(getlasterror)+', '+inttostr(byteswritten)+' bytes written');
-       resp:='error '+inttostr(getlasterror);
-     end;
-  end;
-  if valuetype=vt_qword then
-  begin
-     log('valuetype=qword');
-     bufqword:=strtoint64(value);
-     if writeprocessmemory(target,pointer(addr),@bufqword,vlength,byteswritten) then
-     begin
-       log('wpm ok, '+inttostr(byteswritten)+' bytes written');
-       resp:='ok'
-     end
-     else
-     begin
-       log('wpm error'+inttostr(getlasterror)+', '+inttostr(byteswritten)+' bytes written');
-       resp:='error '+inttostr(getlasterror);
-     end;
-  end;
-  if valuetype=vt_string then
-  begin
-     log('valuetype=string');
-     bufstring:=value;
-     if writeprocessmemory(target,pointer(addr),@bufstring,vlength,byteswritten) then
-     begin
-       log('wpm ok, '+inttostr(byteswritten)+' bytes written');
-       resp:='ok'
-     end
-     else
-     begin
-       log('wpm error'+inttostr(getlasterror)+', '+inttostr(byteswritten)+' bytes written');
-       resp:='error '+inttostr(getlasterror);
-     end;
-  end;
-  result:=resp;
-  log('result: '+result);
+       result:='ok';
+  end
+  else log('error wpm: '+inttostr(getlasterror));
   if not alreadyopened then closehandle(target);
-  log('leaving writemem');
+  log('exit writemem');
 end;
 
 end.
